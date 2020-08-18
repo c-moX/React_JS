7e3927fdccdd405a41d844ca52bfcfad0a5fcd8c
sp simox <69765697+c-moX@users.noreply.github.com>
2020-08-18 13:34:04 +0600

    thrid commit

---
+++ build/precache-manifest.68760ea8e79bd1c67c8f2a6e8b72bf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371ea2efb0582290b52d999b90de3115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709ff1c9b95376fc7271",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62f917cb7c1cf0b48e96",
+    "url": "/static/js/2.276d83ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.276d83ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709ff1c9b95376fc7271",
+    "url": "/static/js/main.9f102d6e.chunk.js"
+  },
+  {
+    "revision": "229ef366b08e7d9170df",
+    "url": "/static/js/runtime-main.3408d7c5.js"
+  }
+]);
